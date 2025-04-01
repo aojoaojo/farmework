2dd7b8588dc3f0aa11f798890d1d27a7a245cc17
aojoaojo <aojoaojov@gmail.com>
2025-04-01 11:14:09 -0300

    deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: "/farmework/",
-  server: {
-    origin: "https://aojoaojo.github.io",
-  },
   plugins: [react()],
 });
